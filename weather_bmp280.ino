4a45f32d495e6fa98f4de9801dba2889d5076347
DrKuebel <jan@thauer.org>
2018-02-15 22:29:58 +0100

    Update and rename 1602.ino to weather_bmp280.ino

--- weather_bmp280.ino
+++ weather_bmp280.ino
@@ -6,25 +6,23 @@ BME280 bme280;
 
 int interval = 5000;
 
-// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
-  // set up the number of columns and rows on the LCD 
   lcd.begin(16, 2);
 
   if(!bme280.init()){
- lcd.print("Device error!");
+  lcd.print("Device error!");
 }
 }
 
 void loop() {
   
   lcd.setCursor(0, 0);
- lcd.print("Temperatur");
+  lcd.print("Temperatur");
   lcd.setCursor(0, 1);
- lcd.print(bme280.getTemperature(),1);
- lcd.print(" C");
+  lcd.print(bme280.getTemperature(),1);
+  lcd.print(" C");
 
  delay(interval);
  lcd.clear();
@@ -41,8 +39,8 @@ lcd.setCursor(0, 0);
  lcd.setCursor(0, 0);
  lcd.print("Luftfeuchtigkeit");
  lcd.setCursor(0, 1);
- lcd.print(bme280.getHumidity());
- lcd.print("%");
+ lcd.print(bme280.getHumidity(),1);
+ lcd.print(" %");
 
  delay(interval);
  lcd.clear();

daa26fa0974939754c563074c464571b854be8cb
DrKuebel <jan@thauer.org>
2018-02-19 18:53:18 +0100

    Update weather_bmp280.ino

--- weather_bmp280.ino
+++ weather_bmp280.ino
@@ -12,7 +12,9 @@ void setup() {
   lcd.begin(16, 2);
 
   if(!bme280.init()){
-  lcd.print("Device error!");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Missing Sensor!");
   }
   
   lcd.setCursor(0, 0);
